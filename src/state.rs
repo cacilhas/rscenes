82509d586adb694ebdeae7f6f119978664445f07
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2023-06-05 01:49:27 -0300

    better docs

--- src/state.rs
+++ src/state.rs
@@ -1,12 +1,12 @@
 /// Used to tell the manager what to do next.
 #[derive(Debug)]
 pub enum State<R> {
-    /// Keep tells to keep the current scene running.
+    /// Tells to keep the current scene running.
     Keep,
 
-    /// New adds a new scene to the stack, switching to it.
+    /// Adds a new scene to the stack, switching to it.
     New(Box<dyn crate::scene::Scene<R>>),
 
-    /// Previous unstack `usize` scenes.
+    /// Unstacks `usize` scenes.
     Previous(usize),
 }
