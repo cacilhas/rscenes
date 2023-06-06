d4456b0dbf858335896821f48321c4cfc6dbec3d
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2023-06-06 00:55:44 -0300

    include_bytes!() returns &'static [u8]

--- src/mem.rs
+++ src/mem.rs
@@ -7,8 +7,9 @@ use raylib::prelude::*;
 /// Returns a Wave object from memory raw data.
 /// `filetype` must be the type extension, including the dot.
 /// For instance: `".wav"`
-pub fn load_wave(filetype: &str, bytes: &Vec<u8>) -> Result<Wave, String> {
-    unsafe { load_wave_from_mem(filetype, bytes, bytes.len() as i32) }
+pub fn load_wave(filetype: &'static str, bytes: &'static [u8]) -> Result<Wave, String> {
+    let bytes = bytes.iter().map(|e| e.to_owned()).collect::<Vec<u8>>();
+    unsafe { load_wave_from_mem(filetype, &bytes, bytes.len() as i32) }
 }
 
 unsafe fn load_wave_from_mem(filetype: &str, bytes: &Vec<u8>, size: i32) -> Result<Wave, String> {
