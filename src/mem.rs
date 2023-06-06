bb253651271d17ed8b0ee36f70d5ef31c316ca2d
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2023-06-06 10:35:56 -0300

    add mem::load_image()

--- src/mem.rs
+++ src/mem.rs
@@ -2,7 +2,15 @@ use std::ffi::CString;
 
 use raylib::prelude::*;
 
-// TODO: identify other *_from_mem functions missing in raylib 3.7
+/// Returns an Image from memory ray data.
+/// For `filetype`, use `mem::ImageType`.
+pub fn load_image(
+    filetype: impl Into<&'static str>,
+    bytes: &'static [u8],
+) -> Result<Image, String> {
+    let bytes = bytes.iter().map(|e| e.to_owned()).collect::<Vec<u8>>();
+    Image::load_image_from_mem(filetype.into(), &bytes, bytes.len() as i32)
+}
 
 /// Returns a Wave object from memory raw data.
 /// For `filetype`, use `mem::WaveType`.
@@ -48,6 +56,37 @@ impl From<WaveType> for &'static str {
             WaveType::Opus => ".opus",
             WaveType::Qoa => ".qoa",
             WaveType::Wav => ".wav",
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum ImageType {
+    Bitmap,
+    Heif,
+    Jpeg,
+    Pixmap,
+    Png,
+    Pnm,
+    Raw,
+    Svg,
+    Tiff,
+    WebP,
+}
+
+impl From<ImageType> for &'static str {
+    fn from(value: ImageType) -> Self {
+        match value {
+            ImageType::Bitmap => ".bmp",
+            ImageType::Heif => ".heif",
+            ImageType::Jpeg => ".jpg",
+            ImageType::Pixmap => ".xpm",
+            ImageType::Png => ".png",
+            ImageType::Pnm => ".pnm",
+            ImageType::Raw => ".raw",
+            ImageType::Svg => ".svg",
+            ImageType::Tiff => ".tiff",
+            ImageType::WebP => ".webp",
         }
     }
 }
