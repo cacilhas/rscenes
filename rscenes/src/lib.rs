5e494feee0a6e0751c2aaaac198a253b58007ad6
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2024-01-28 13:14:34 -0300

    add macros

--- rscenes/src/lib.rs
+++ rscenes/src/lib.rs
@@ -6,5 +6,6 @@ pub mod prelude {
     pub use crate::connectors::{Connector2D, Connector3D};
     pub use crate::scene::Scene;
     pub use crate::state::State;
+    pub use rscenes_macros::draw;
     pub use rscenes_raylib_connector::assets::*;
 }
