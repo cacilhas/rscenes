b360fbe42313aeb9f67991fac2b001c744a05ef5
Montegasppα Cacilhας <montegasppa@cacilhas.info>
2024-02-04 02:52:55 -0300

    show version

--- nonogram/src/ui/main_menu.rs
+++ nonogram/src/ui/main_menu.rs
@@ -125,6 +125,11 @@ impl Scene for MainMenu {
             y: 2.0,
         };
         rl.draw_text_ex(font, "Nonogram", position, 84.0, 2.0, TITLE);
+        let position = position.add(Vector2 {
+            x: size.x + 8.0,
+            y: 10.0,
+        });
+        rl.draw_text_ex(font, "v. 4.0", position, 20.0, 2.0, FOREGROUND);
 
         if self.easy {
             rl.draw_rectangle_rec(self.buttons[LB_EASY], EZCOLOR);
